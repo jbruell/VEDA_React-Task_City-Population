d5adb2c57b0d389b1520f5cabd7866c48957c39b
Janik Brüll <janik.bruell@veda.net>
2021-09-08 11:48:20 +0200

    Calculate population difference

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Autocomplete from './components/Autocomplete';
+import { debounce } from './debounce';
 
 function App() {
+  const [city1, setCity1] = useState(null);
+  const [city2, setCity2] = useState(null);
+
+  const fetchPopulation = async (city, setter) => {
+    if (!city) {
+      return;
+    }
+    const res = await fetch(city._links["city:item"].href);
+
+    if (res.status === 200) {
+      const body = await res.json();
+      setter({ ...city, population: body.population });
+    }
+  }
+
+  useEffect(debounce(async () => fetchPopulation(city1, setCity1), 250), [city1]);
+  useEffect(debounce(async () => fetchPopulation(city2, setCity2), 250), [city2]);
+
   return (
     <div className="App">
-      <Autocomplete />
+      <h2>City 1</h2>
+      <div className="info-display">
+        {city1 ?
+          <>
+            <span>Name: {city1?.matching_full_name}</span>
+            <span>Population: {city1?.population}</span>
+          </>
+          :
+          <span>-</span>
+        }
+      </div>
+      <h2>City 2</h2>
+      <div className="info-display">
+        {city2 ?
+          <>
+            <span>Name: {city2?.matching_full_name}</span>
+            <span>Population: {city2?.population}</span>
+          </>
+          :
+          <span>-</span>
+        }
+      </div>
+      {city1?.population && city2?.population &&
+        <span>Population difference: {Math.abs(city1?.population - city2?.population)}</span>
+      }
+      <div className="autocomplete-wrapper">
+        <span>Choose city 1:&nbsp;&nbsp;</span>
+        <Autocomplete onCityClick={city => setCity1(city)} />
+      </div>
+      <div className="autocomplete-wrapper">
+        <span>Choose city 2:&nbsp;&nbsp;</span>
+        <Autocomplete onCityClick={city => setCity2(city)} />
+      </div>
     </div>
   );
 }
--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -4,14 +4,13 @@ import "./Autocomplete.css";
 
 const API_URL = "https://api.teleport.org/api/cities?limit=5&search=";
 
-const Autocomplete = () => {
+const Autocomplete = ({ onCityClick }) => {
     const [input, setInput] = useState("");
     const [inFlight, setInFlight] = useState(false);
     const [cityChoices, setCityChoices] = useState([]);
 
     useEffect(() => {
         if (!inFlight && input.length > 0) {
-            console.log("trigger")
             fetchCitiesByString();
         }
     }, [input])
@@ -22,32 +21,30 @@ const Autocomplete = () => {
 
         if (res.status === 200) {
             const body = await res.json();
-            setCityChoices(body?._embedded["city:search-results"])
-            console.log(body?._embedded["city:search-results"]);
+            setCityChoices(body?._embedded["city:search-results"]);
         }
         setInFlight(false);
     }, 250);
     return (
-        <div>
-            <div className="city-info">
-                <span>
-                    {"" +inFlight}
-                </span>
-                <span>
-                </span>
-            </div>
+        <span>
             <input type="text"
                 value={input}
                 onChange={event => {
                     setInput(event.target.value);
                 }} />
-            {cityChoices && cityChoices.length > 1 && 
+            {cityChoices && cityChoices.length > 0 && 
             <ul className="city-choices">
                 {cityChoices.map(c => (
-                    <li key={c.matching_full_name}>{c.matching_full_name}</li>
+                    <li key={c.matching_full_name}
+                        onClick={() => {
+                            onCityClick(c);
+                            setCityChoices(null);
+                        }}>
+                            {c.matching_full_name}
+                    </li>
                 ))}
             </ul>}
-        </div>
+        </span>
     )
 }
 
